486f681417a5884476fc2890d86600ba4070db3b
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2021-11-01 11:44:56 +0100

    Started creating profile view. Edited material theme.

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -3,20 +3,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:library_locator/review_card.dart';
 
 class DatabaseService {
-  final database = FirebaseDatabase.instance.reference();
+  final firebaseDatabase = FirebaseDatabase.instance.reference();
   List<Widget> reviewList = <Widget>[];
 
-  Future<List<Widget>> setReviews() async {
-    final reviews = database.child("books/12345/reviews/");
+  /// Get all the reviews for a book on the given isbn number
+  Future<List<Widget>> getAllReviewsForBook(String isbn) async {
+    final reviews = firebaseDatabase.child("books/"+ isbn +"/reviews/");
     Map<dynamic, dynamic> data = <dynamic, dynamic>{};
     await reviews.get().then((DataSnapshot snapshot) {
       data = new Map<dynamic, dynamic>.from(snapshot.value);
       data.forEach((key, value) {
         // Here key is the reviewers Name/ID and the value consists of the star rating and text in the review
         List<String> listOfReviewContent =
-            value.toString().replaceAll("}", "").split(",");
+        value.toString().replaceAll("}", "").split(",");
         String reviewText =
-            listOfReviewContent.elementAt(1).split("text: ").elementAt(1);
+        listOfReviewContent.elementAt(1).split("text: ").elementAt(1);
         double reviewStars = double.parse(
             listOfReviewContent.elementAt(0).split("stars: ").elementAt(1));
         reviewList.add(new ReviewCard(
@@ -26,4 +27,23 @@ class DatabaseService {
     return reviewList;
   }
 
+
+  Future<double> getAverageRating(String isbn) async {
+    double averageRating = 0;
+    final reviews = firebaseDatabase.child("books/"+ isbn +"/reviews/");
+    Map<dynamic, dynamic> data = <dynamic, dynamic>{};
+    await reviews.get().then((DataSnapshot snapshot) {
+      data = new Map<dynamic, dynamic>.from(snapshot.value);
+      double totalRating = 0;
+      data.forEach((key, value) {
+        // Here key is the reviewers Name/ID and the value consists of the star rating and text in the review
+        List<String> listOfReviewContent = value.toString().replaceAll("}", "").split(",");
+        double reviewStars = double.parse(listOfReviewContent.elementAt(0).split("stars: ").elementAt(1));
+        totalRating += reviewStars;
+      });
+      averageRating = totalRating/data.length;
+    });
+
+    return averageRating;
+  }
 }
