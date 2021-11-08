71dc0f8c672d1a1f6d70bd6fe8c72a6a6d5503d5
Hans <hansal@ntnu.no>
2021-11-08 13:44:55 +0100

    Added bottomNavigationBar to the app it is always shown

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:library_locator/profile_view.dart';
+import 'package:library_locator/providers/bottom_navigation_provider.dart';
 
+import 'book_details_view.dart';
+import 'bottom_navigation_bar_widget.dart';
 import 'home.dart';
 import 'login_view.dart';
 
+class Logger extends ProviderObserver {
+  @override
+  void didUpdateProvider(ProviderBase provider, Object? newValue) {
+    print('''
+{
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "$newValue"
+}''');
+  }
+}
+
 void main() {
-  runApp(App());
+  runApp(ProviderScope(observers: [
+    Logger(),
+  ], overrides: [], child: App()));
 }
 
 class App extends StatefulWidget {
@@ -19,133 +35,59 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
+  Widget _getViewContainer(int index) {
+
+    final _currentUser = FirebaseAuth.instance.currentUser;
+
+    List<Widget> notLoggedIn = [HomePage(), BookDetailsView(isbn: "12345"), LoginPage()];
+    List<Widget> userWidgetsList = [HomePage(), BookDetailsView(isbn: "12345"), ProfilePage()];
+
+
+    if (_currentUser != null) {
+        return userWidgetsList[index];
+    } else {
+      return notLoggedIn[index];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error);
-        }
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+          }
 
-        // After initialization, app is shown
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            title: 'Library Locator',
-            theme: ThemeData(
-              brightness: Brightness.light,
-              primarySwatch: Colors.pink,
+          // After initialization, app is shown
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Library Locator',
+              theme: ThemeData(
+                brightness: Brightness.light,
+                primarySwatch: Colors.pink,
               ),
-            home: HomePage(),
-          );
-        }
+              home: Scaffold(
+                body: Consumer(builder: (context, watch, child) {
+                  final _indexState = watch(bottomNavigationBarIndexProvider);
+                  return SafeArea(
+                    child: _getViewContainer(_indexState),
+                  );
+                },),
+                bottomNavigationBar: BottomNavigationBarWidget(),
+              ),
+            );
+          }
 
-        // While app is initializing
-        return LoadingScreen();
-      }
-    );
+          // While app is initializing
+          return LoadingScreen();
+        });
   }
-
 }
 
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Text('Loading...')
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  int _counter = 0;
-  String _barcode = "";
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  static Future<void> openMap(double latitude, double longitude) async {
-    String googleUrl = 'https://www.google.com/maps/dir/?api=1&origin=Your+location&destination=NTNU+Biblioteket+i+%C3%85lesund,+Larsg%C3%A5rdsvegen+bygget+2&travelmode=walking';
-
-      await launch(googleUrl);
-  }
-
-  Future<void> barcodeScanner() async {
-      _barcode = await FlutterBarcodeScanner.scanBarcode("#101010", "Cancel", false, ScanMode.BARCODE);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Barcode scanned:',
-            ),
-            Text(
-              '$_barcode',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => LoginPage()),
-          );
-          },
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return MaterialApp(home: Text('Loading...'));
   }
 }
