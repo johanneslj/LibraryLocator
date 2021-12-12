4323b1b1e2270ed6e55f04c3ce7da070fc20222b
Hans <hansal@ntnu.no>
2021-12-12 16:50:31 +0100

    made stars align left on cards

--- lib/reviews/book_card.dart
+++ lib/reviews/book_card.dart
@@ -12,8 +12,8 @@ class BookCard extends StatefulWidget {
   final String author;
   final String summary;
 
-
-  const BookCard({Key? key, required this.isbn, required this.imageURL, required this.stars, required this.title, required this.author,required this.summary})
+  const BookCard(
+      {Key? key, required this.isbn, required this.imageURL, required this.stars, required this.title, required this.author, required this.summary})
       : super(key: key);
 
   @override
@@ -21,8 +21,12 @@ class BookCard extends StatefulWidget {
 }
 
 class _BookCardState extends State<BookCard> {
-  Image noImage = Image.asset("assets/book.jpg", height: 90, width: 90,);
-  
+  Image noImage = Image.asset(
+    "assets/book.jpg",
+    height: 90,
+    width: 90,
+  );
+
   Widget build(BuildContext context) {
     double averageRating = 0;
     return FutureBuilder<double>(
@@ -37,56 +41,63 @@ class _BookCardState extends State<BookCard> {
               averageRating = snapshot.data!;
             return InkWell(
               onTap: () {
-                pushToDetails(widget.isbn,widget.title,widget.author,widget.imageURL,widget.summary);
+                pushToDetails(widget.isbn, widget.title, widget.author, widget.imageURL, widget.summary);
               },
               child: Row(
                 children: [
                   Expanded(
-                  child: Container(
-                    child: Padding(
-                        padding: EdgeInsets.fromLTRB(5, 2, 0, 2),
-                        child: Container(
-                            child: Row(children: [
-                              Image(image: NetworkImage(widget.imageURL), height: 90, width: 90,
-                              errorBuilder: (BuildContext context, Object e, StackTrace? stackTrace) {
-                                return noImage;
-                              }),
-                              Expanded(
+                    child: Container(
+                      child: Padding(
+                          padding: EdgeInsets.fromLTRB(5, 2, 0, 2),
+                          child: Container(
+                              child: Row(children: [
+                            Image(
+                                image: NetworkImage(widget.imageURL),
+                                height: 90,
+                                width: 90,
+                                errorBuilder: (BuildContext context, Object e, StackTrace? stackTrace) {
+                                  return noImage;
+                                }),
+                            Expanded(
                                 child: Padding(
-                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Column(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: Column(
+                                children: [
+                                  Text(widget.title, overflow: TextOverflow.ellipsis, textAlign: TextAlign.left, style: TextStyle(fontSize: 22)),
+                                  Text(widget.author, overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 22)),
+                                  Row(
                                     children: [
-                                      Text(widget.title, overflow: TextOverflow.ellipsis, textAlign: TextAlign.left, style: TextStyle(fontSize: 22)),
-                                      Text(widget.author, overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 22)),
-                                      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                                        Text("(" + averageRating.toString().substring(0, 3) + ")", style: TextStyle(fontSize: 16)),
-                                        RatingBarIndicator(
-                                          rating: averageRating,
-                                          itemBuilder: (context, index) => Icon(
-                                            Icons.star,
-                                            color: Colors.amber,
-                                          ),
-                                          itemCount: 5,
-                                          itemSize: 20,
+                                      Text("(" + averageRating.toString().substring(0, 3) + ")", style: TextStyle(fontSize: 16)),
+                                      RatingBarIndicator(
+                                        rating: averageRating,
+                                        itemBuilder: (context, index) => Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
                                         ),
-                                      ]),
+                                        itemCount: 5,
+                                        itemSize: 20,
+                                      ),
                                     ],
-                                    crossAxisAlignment: CrossAxisAlignment.start,
                                     mainAxisAlignment: MainAxisAlignment.start,
                                   ),
-                                )
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
                               ),
-                            ]))),
-                  ),
-                )],
+                            )),
+                          ]))),
+                    ),
+                  )
+                ],
               ),
             );
           }
         });
   }
 
-  void pushToDetails(String isbn,String title,String author,String imageURL,String summary) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => BookDetailsView(isbn: isbn,title:title,author:author,imageURL:imageURL,summary:summary)));
+  void pushToDetails(String isbn, String title, String author, String imageURL, String summary) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => BookDetailsView(isbn: isbn, title: title, author: author, imageURL: imageURL, summary: summary)));
   }
 
   void initState() {
